57cb83de63e4bbd0a84f74abcdb1306722f7b795
unknown <mstuhin>
2022-03-26 10:15:36 +0600

    firstcommit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Shop.css'
+
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h3>This is for products</h3>
+            </div>
+            <h3>Order Summary</h3>
+            <div className="cart-container">
+
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
