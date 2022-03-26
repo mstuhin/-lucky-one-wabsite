5f2ebcc7f0d0e0e5b5130f16a55dac259cc59d6a
unknown <mstuhin>
2022-03-26 22:40:39 +0600

    addcartbutton

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Product.css'
+
 
 const Product = (props) => {
     const { name, picture, price, id } = props.product;
     return (
-        <div>
+        <div className='product'>
             <img src={picture} alt=""></img>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price:$ {price}</p>
+                <p>ID:{id}</p>
+            </div>
+
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
+
         </div>
     );
 };
